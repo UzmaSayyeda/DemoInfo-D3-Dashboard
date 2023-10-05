577c17246ac5b6574e0007aaa261cc688cd01dde
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-10-05 14:30:20 -0500

    updated bonus and cleaned code, added favicon

--- StarterCode/static/js/bonus.js
+++ StarterCode/static/js/bonus.js
@@ -1,131 +1,92 @@
-function Gauge(sample){
-    d3.json(url).then((data) => {
+// **
+//  * BONUS Solution
+//  * */
+function buildGauge(wfreq) {
+  // Enter the washing frequency between 0 and 180
+  let level = parseFloat(wfreq) * 20;
 
-        let metadata = data.metadata;
-        
-        let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-        let result = resultArray[0];
-        let washFreq = result.wfreq;
+  // Trig to calc meter point
+  let degrees = 180 - level;
+  let radius = 0.5;
+  let radians = (degrees * Math.PI) / 180;
+  let x = radius * Math.cos(radians);
+  let y = radius * Math.sin(radians);
 
+  // Path: may have to change to create a better triangle
+  let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+  let pathX = String(x);
+  let space = " ";
+  let pathY = String(y);
+  let pathEnd = " Z";
+  let path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-// gauge
-
-        let gauagedata = [
-            {
-                mode: "gauge",
-                type: "indicator",
-                value: washFreq,
-        
-                gauge: {
-                    shape: "angular",
-                    bar: {
-                        color: "blue",
-                        line: {
-                            color: "red",
-                            width: 4
-                        },  
-                        thickness: 0
-                    },
-                    bgcolor: "#388",
-                    bordercolor: "#a89d32",
-                    borderwidth: 3,
-                    axis: {
-                        range: [0,9],
-                        visible: true,
-                        tickmode: "array",
-                        tickvals: [0,1,2,3,4,5,6,7,8,9],
-                        ticks: "outside"
-                    },
-                    
-                        
-                            
-                    steps: [
-                    { range: [0, 1], color: "#f8f3ec" },
-                    { range: [1,2], color: "#f4f1e4" },
-                    { range: [2,3], color: "#e9e7c9" },
-                    { range: [3,4], color: "#e5e8b0" },
-                    { range: [4,5], color: "#d5e599" },
-                    { range: [5,6], color: "#b7cd8f" },
-                    { range: [6,7], color: "#8bc086" },
-                    { range: [7,8], color: "#89bc8d" },
-                    { range: [8,9], color: "#84b589" }
-                    
-                    ]
-                        
-                    
-                
-              }    }
+  let data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      textinfo: "text",
+      textposition: "inside",
+      marker: {
+        colors: [
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
         ]
-        
-        var theta = 93.5
-        var r = 0.7
-        var x_head = r * Math.cos(Math.PI/180*theta)
-        var y_head = r * Math.sin(Math.PI/180*theta)
-        
-        let layout = {
-          xaxis: {range: [0, 1], showgrid: false, 'zeroline': false, 'visible': false},
-          yaxis: {range: [0, 1], showgrid: false, 'zeroline': false, 'visible': false},
-          showlegend: false,
-          annotations: [
-            {
-              ax: 0.5,
-              ay: 0,
-              axref: 'x',
-              ayref: 'y',
-              x: 0.5+x_head,
-              y: y_head,
-              xref: 'x',
-              yref: 'y',
-              showarrow: true,
-              arrowhead: 9,
-            }
-          ]
-        };
-        Plotly.newPlot('gauge', gauagedata, layout)
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
 
+  let layout = {
+    shapes: [
+      {
+        type: "path",
+        path: path,
+        fillcolor: "850000",
+        line: {
+          color: "850000"
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    },
+    yaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    }
+  };
 
-
-
-
-
-
-
-
-
-
-
-// var data = [
-//     {
-//       domain: { x: [0, 1], y: [0, 1] },
-//       value: washFreq,
-//       title: { text: "Belly Button Washing Frequency" },
-//       type: "indicator",
-//       mode: "gauge+number",
-//       labels:["0-1","1-2","2-3","3-4","4-5","5-6","6-7","7-8","8-9"],
-//       text:["0-1","1-2","2-3","3-4","4-5","5-6","6-7","7-8","8-9"],
-//       textInfo : "text", 
-//       gauge: {
-        
-//         axis: { range: [null, 9] },
-//         steps: [
-//           { range: [0, 1], color: "#f8f3ec" },
-//           { range: [1,2], color: "#f4f1e4" },
-//           { range: [2,3], color: "#e9e7c9" },
-//           { range: [3,4], color: "#e5e8b0" },
-//           { range: [4,5], color: "#d5e599" },
-//           { range: [5,6], color: "#b7cd8f" },
-//           { range: [6,7], color: "#8bc086" },
-//           { range: [7,8], color: "#89bc8d" },
-//           { range: [8,9], color: "#84b589" }
-          
-//         ]
-       
-        
-//       }
-//     }
-//   ];
-  
-//   var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-//   Plotly.newPlot('gauge', data, layout)
-
- })};
+  // let GAUGE = document.getElementById("gauge");
+  Plotly.newPlot("gauge", data, layout);
+}
